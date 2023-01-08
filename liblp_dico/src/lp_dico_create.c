0254039db9a40b3efadc9234b7b13ca11053b7f9
lperroti <lperroti@e2r8p6.clusters.42paris.fr>
2023-01-08 18:50:10 +0100

    debug dico

--- liblp_dico/src/lp_dico_create.c
+++ liblp_dico/src/lp_dico_create.c
@@ -6,21 +6,16 @@
 /*   By: lperroti <lperroti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 01:27:16 by lperroti          #+#    #+#             */
-/*   Updated: 2023/01/08 15:04:33 by lperroti         ###   ########.fr       */
+/*   Updated: 2023/01/08 18:33:07 by lperroti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../liblp_dico.h"
 
-t_dico	*lp_dico_create(void)
+t_dico	lp_dico_create(void)
 {
-	t_dico	*new_dico;
+	t_dico	new_dico;
 
-	new_dico = malloc(sizeof(t_dico) + 255 * MAX_KEY_LENGTH);
-
-	*new_dico = (t_dico){
-		.firstelem = NULL,
-		.hashmap = (t_dico *)new_dico + 1,
-	};
+	new_dico.firstelem = NULL;
 	return (new_dico);
 }
