2abaf3a8567fb7ba5fd8d53ebbed7da49c71db62
Elnop <elnop@dev.fr>
2023-03-21 19:39:47 +0100

    debug strcat

--- liblp_str/src/lp_strcat.c
+++ liblp_str/src/lp_strcat.c
@@ -6,25 +6,27 @@
 /*   By: lperroti <lperroti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:46:05 by lperroti          #+#    #+#             */
-/*   Updated: 2023/01/11 11:51:52 by lperroti         ###   ########.fr       */
+/*   Updated: 2023/03/21 19:38:50 by lperroti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "../liblp_str.h"
 
 bool	lp_strcat(char **s1, char const *s2)
 {
 	char	*str;
+	char	*cp_s1;
 	int		i;
 
 	str = malloc((lp_strlen(*s1) + lp_strlen(s2) + 1) * sizeof(char));
 	if (!str)
 		return (false);
 	i = 0;
-	while (*s1)
+	cp_s1 = *s1;
+	if (!cp_s1)
+		cp_s1 = "";
+	while (*cp_s1)
 	{
-		str[i++] = **s1;
-		(*s1)++;
+		str[i++] = *cp_s1;
+		cp_s1++;
 	}
 	free(*s1);
 	while (*s2)
