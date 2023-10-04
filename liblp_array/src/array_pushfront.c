239e3f4c8758f851aa0e79ee402039e275e167b0
lperroti <lperroti.dev@gmail.com>
2023-10-04 20:19:12 +0200

    debug

--- liblp_array/src/array_pushfront.c
+++ liblp_array/src/array_pushfront.c
@@ -6,13 +6,13 @@
 /*   By: lperroti <lperroti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 13:00:42 by lperroti          #+#    #+#             */
-/*   Updated: 2023/09/07 20:50:48 by lperroti         ###   ########.fr       */
+/*   Updated: 2023/10/04 20:17:50 by lperroti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../liblp_array.h"
 
-t_array	array_pushfront(t_array	*parray, void	*elem)
+t_array	array_pushfront(t_array	*parray, void	*pelem)
 {
 	size_t	size;
 	size_t	elemsize;
@@ -23,11 +23,10 @@ t_array	array_pushfront(t_array	*parray, void	*elem)
 		array_grow(parray, 1);
 	if (size)
 		lp_memmove((char *)*parray + elemsize, *parray, size * elemsize);
-	if (array_header(*parray)->copy_elem
-		&& !array_header(*parray)->copy_elem(elem, *parray))
-		return (NULL);
+	if (array_header(*parray)->copy_elem)
+		(array_header(*parray)->copy_elem(pelem, *parray));
 	else
-		lp_memcpy(*parray, elem, elemsize);
+		lp_memcpy(*parray, pelem, elemsize);
 	array_set_size(*parray, size + 1);
 	return (*parray);
 }
